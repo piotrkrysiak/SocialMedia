a041b09001638cbb040dabb0f9bc58c19ba15414
piotrkrysiak <plotrekhqpl@gmail.com>
2021-11-30 23:03:04 +0100

    MongoDB connection added

--- index.ts
+++ index.ts
@@ -1,4 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+if (process.env.MONGO_URL) {
+  mongoose.connect(process.env.MONGO_URL, {}, () =>
+    console.log("Connected to MongoDB")
+  );
+}
 
 const app = express();
 const PORT = 8000;
@@ -6,3 +16,4 @@ app.get("/", (req, res) => res.send("Express + TypeScript Server"));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
+
