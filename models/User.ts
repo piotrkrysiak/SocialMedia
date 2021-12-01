09bf5a50dcc38f8de1ace5d27cbd4c2b2290b557
piotrkrysiak <plotrekhqpl@gmail.com>
2021-12-01 13:56:41 +0100

    Login and Register system implemented

--- models/User.ts
+++ models/User.ts
@@ -1,45 +1,64 @@
-import mongoose from "mongoose";
+import { Schema, model, Document } from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    min: 3,
-    max: 20,
-    unique: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    min: 5,
-    max: 64,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    min: 6,
-  },
-  profilePicture: {
-    type: String,
-    default: "",
-  },
-  coverPicture: {
-    type: String,
-    default: "",
-  },
-  followers: {
-    type: [Number],
-    default: [],
-  },
-  following: {
-    type: [Number],
-    default: [],
-  },
-  isAdmin: {
-    type: Boolean,
-    default: false,
-  },
-});
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  profilePicture?: string;
+  coverPicture?: string;
+  followers?: number[];
+  following?: number[];
+  isAdmin?: boolean;
+  description?: string;
+}
 
-export default mongoose.model("User", UserSchema);
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 20,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      min: 5,
+      max: 64,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    coverPicture: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: [Number],
+      default: [],
+    },
+    following: {
+      type: [Number],
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+export default model<IUser>("User", UserSchema);
--- routes/auth.ts
+++ routes/auth.ts
@@ -1,10 +1,38 @@
 import express from "express";
+import User, { IUser } from "../models/User";
+import bcrypt from "bcrypt";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Hi it is auth route");
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hiddenPassword = await bcrypt.hash(req.body.password, salt);
+    const user: IUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hiddenPassword,
+    });
+    const savedUser = await user.save();
+    res.status(200).json(savedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("User not found");
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user!.password
+    );
+    !validPassword && res.status(400).json("Invalid credentials");
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 export default router;
-
