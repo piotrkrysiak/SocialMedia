7f9cd3ad48420268086abaaec9c94f32c87434b3
piotrkrysiak <plotrekhqpl@gmail.com>
2021-12-02 20:58:38 +0100

    nested async fetch & basic API version finished

--- routes/posts.ts
+++ routes/posts.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import Post from "../models/Post";
+import User from "../models/User";
 
 const router = Router();
 
@@ -62,6 +63,21 @@ router.get("/:id", async (req, res) => {
     res.status(200).json(post);
   } catch (error) {
     res.status(500).json(error);
+  }
+});
+
+router.get("/timeline/all", async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser!._id });
+    const friendPosts = await Promise.all(
+      currentUser!.following!.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    res.json(userPosts.concat(...friendPosts));
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
